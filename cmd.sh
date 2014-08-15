aa839d88359106b6daab76560922719441e2fca7
Karthik Gururaj <karthik.gururaj@intel.com>
2014-08-15 09:50:49 -0700

    Edited cmd.sh file for merging

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,8 @@
-./bcftools merge  --merge-config=config.sample --gatk -i BaseQRankSum:median,MQ:median,MQ0:median,ClippingRankSum:median,MQRankSum:median,ReadPosRankSum:median,DP:sum -m all --gvcf --reference=test_inputs/Homo_sapiens_assembly19.fasta $@
+if [ "$#" -lt 2 ];
+then
+  echo "Needs at least 2 arguments  <reference_file>  '-l <file_list>'|'<vcf1> <vcf2> ...'";
+  exit
+fi
+reference_file=$1;
+shift
+./bcftools merge  --merge-config=config.sample --gatk -i BaseQRankSum:median,MQ:median,MQ0:median,ClippingRankSum:median,MQRankSum:median,ReadPosRankSum:median,DP:sum -m all --gvcf --reference=${reference_file} $@
